471b5f1804d6ae476497ce26047af5779cba4b1f
abma2008 <abma2008@gmail.com>
2023-05-14 00:53:00 +0400

    adding promises.js file

---
+++ 1_10_tutorial/promises.js
@@ -0,0 +1,11 @@
+/**
+ * This is an explanation, basic, about promises.
+ * we will be creating some functions to comprehend the concept more.
+ * As we move on, we will be adding more code to understand
+ * how ansyc and await work....
+ * this is a very important tutorial and it can be of a great help in the future.
+ * 
+ * Please follow the example below:
+ * ===================================================
+ */
+
